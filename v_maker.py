270454a2041441d086d83580e63d35d708dbbfbc
Varun Raj Panigrahy <35497504+VarunRajPanigrahy@users.noreply.github.com>
2019-09-29 14:50:09 +0530

    Update v_maker.py

--- v_maker.py
+++ v_maker.py
@@ -3,29 +3,30 @@ import cv2
 import time
 
 # The duration in seconds of the video captured
-x=0
-star_time=time.time()
-while(True):
-	end_time=time.time()
-	if(int(end_time - star_time)>40):break
-	capture_duration = 5
+def make_video():
+	x=0
+	star_time=time.time()
+	while(True):
+		end_time=time.time()
+		if(int(end_time - star_time)>20):break
+		capture_duration = 5
 
-	cap = cv2.VideoCapture(0)
-	cap.set(cv2.CAP_PROP_FPS, 1)
-	fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	out = cv2.VideoWriter('Video/output_%d.avi'%(x),fourcc,5.0, (640,480))
-	x+=1
-	start_time = time.time()
-	while( int(time.time() - start_time) < capture_duration ):
-	    ret, frame = cap.read()
-	    if ret==True:
-	        out.write(frame)
-	        
-	        #cv2.imshow('frame',frame)
-	    
-	    else:
-	        break
+		cap = cv2.VideoCapture(0)
+		cap.set(cv2.CAP_PROP_FPS, 1)
+		fourcc = cv2.VideoWriter_fourcc(*'XVID')
+		out = cv2.VideoWriter('Video/output_%d.avi'%(x),fourcc,5.0, (640,480))
+		x+=1
+		start_time = time.time()
+		while( int(time.time() - start_time) < capture_duration ):
+		    ret, frame = cap.read()
+		    if ret==True:
+		        out.write(frame)
+		        
+		        #cv2.imshow('frame',frame)
+		    
+		    else:
+		        break
 
-	cap.release()
-	out.release()
-	cv2.destroyAllWindows()
+		cap.release()
+		out.release()
+		cv2.destroyAllWindows()
