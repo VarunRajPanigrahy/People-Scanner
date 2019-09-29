571a8a4aa6cbe3353aeb85203137b1776c6458cc
Maram Sumanth <maram.sumanth@gmail.com>
2019-09-29 12:23:36 +0530

    Update v_maker.py

--- v_maker.py
+++ v_maker.py
@@ -6,23 +6,19 @@ import time
 x=0
 while(True):
 	capture_duration = 5
-
 	cap = cv2.VideoCapture(0)
 	cap.set(cv2.CAP_PROP_FPS, 1)
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
 	out = cv2.VideoWriter('Video/output_%d.avi'%(x),fourcc,10.0, (640,480))
 	x+=1
 	start_time = time.time()
-	while( int(time.time() - start_time) < capture_duration ):
+	while(int(time.time() - start_time) < capture_duration ):
 	    ret, frame = cap.read()
 	    if ret==True:
 	        out.write(frame)
-	        
 	        #cv2.imshow('frame',frame)
-	    
 	    else:
 	        break
-
 	cap.release()
 	out.release()
 	cv2.destroyAllWindows()
