e92ba0cc2208b058d05e77268883ae9f9592553b
Varun Raj Panigrahy <35497504+VarunRajPanigrahy@users.noreply.github.com>
2019-09-29 12:50:21 +0530

    Update v_maker.py

--- v_maker.py
+++ v_maker.py
@@ -4,13 +4,16 @@ import time
 
 # The duration in seconds of the video captured
 x=0
+star_time=time.time()
 while(True):
+	end_time=time.time()
+	if(int(end_time - star_time)>40):break
 	capture_duration = 5
 
 	cap = cv2.VideoCapture(0)
 	cap.set(cv2.CAP_PROP_FPS, 1)
 	fourcc = cv2.VideoWriter_fourcc(*'XVID')
-	out = cv2.VideoWriter('Video/output_%d.avi'%(x),fourcc,10.0, (640,480))
+	out = cv2.VideoWriter('Video/output_%d.avi'%(x),fourcc,5.0, (640,480))
 	x+=1
 	start_time = time.time()
 	while( int(time.time() - start_time) < capture_duration ):
